1381a6d61252ad8b779febe980912a8f06eb00a5
Varun Tolani <vatolani@gmail.com>
2018-01-26 16:41:51 -0800

    add pwm example

---
+++ frdmk64f_pwm/source/main.c
@@ -0,0 +1,187 @@
+/*
+ * The Clear BSD License
+
+ * Copyright (c) 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2017 NXP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided
+ * that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*System includes.*/
+#include <stdio.h>
+
+/* Freescale includes. */
+#include "fsl_device_registers.h"
+#include "fsl_debug_console.h"
+#include "board.h"
+#include "fsl_ftm.h"
+
+#include "pin_mux.h"
+#include "clock_config.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/* The Flextimer instance/channel used for board. Indicates pin PTC1, see pin view for more info */
+#define BOARD_FTM_BASEADDR FTM0
+#define BOARD_FTM_CHANNEL kFTM_Chnl_0
+
+/* Interrupt number and interrupt handler for the FTM instance used */
+#define FTM_INTERRUPT_NUMBER FTM0_IRQn
+#define FTM_0_HANDLER FTM0_IRQHandler
+
+/* Interrupt to enable and flag to read; depends on the FTM channel used */
+#define FTM_CHANNEL_INTERRUPT_ENABLE kFTM_Chnl0InterruptEnable
+#define FTM_CHANNEL_FLAG kFTM_Chnl0Flag
+
+/* Get source clock for FTM driver */
+#define FTM_SOURCE_CLOCK CLOCK_GetFreq(kCLOCK_BusClk)
+
+//High Voltage(3.3V)=True for pwm
+#define PWM_LEVEL kFTM_HighTrue
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/*!
+ * @brief delay a while.
+ */
+void delay(void);
+void update_duty_cycle(uint8_t updated_duty_cycle);
+void init_pwm(uint32_t freq_hz, uint8_t init_duty_cycle);
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+volatile bool ftmIsrFlag = false;
+volatile uint8_t duty_cycle = 1U;
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+void init_board()
+{
+	/* Board pin, clock, debug console init */
+	BOARD_InitPins();
+	BOARD_BootClockRUN();
+	BOARD_InitDebugConsole();
+}
+
+/*Initialize the Flexible Timer Module to Produce PWM with init_duty_cycle at freq_hz*/
+void init_pwm(uint32_t freq_hz, uint8_t init_duty_cycle)
+{
+	duty_cycle = init_duty_cycle;
+    ftm_config_t ftmInfo;
+    ftm_chnl_pwm_signal_param_t ftmParam;
+
+    /* Configure ftm params with for pwm freq- freq_hz, duty cycle- init_duty_cycle */
+    ftmParam.chnlNumber = BOARD_FTM_CHANNEL;
+    ftmParam.level = PWM_LEVEL;
+    ftmParam.dutyCyclePercent = init_duty_cycle;
+    ftmParam.firstEdgeDelayPercent = 0U;
+
+    FTM_GetDefaultConfig(&ftmInfo);
+    /* Initialize FTM module */
+    FTM_Init(BOARD_FTM_BASEADDR, &ftmInfo);
+
+    FTM_SetupPwm(BOARD_FTM_BASEADDR, &ftmParam, 1U, kFTM_CenterAlignedPwm, freq_hz, FTM_SOURCE_CLOCK);
+
+    /* Enable channel interrupt flag.*/
+    FTM_EnableInterrupts(BOARD_FTM_BASEADDR, FTM_CHANNEL_INTERRUPT_ENABLE);
+
+    /* Enable at the NVIC */
+    EnableIRQ(FTM_INTERRUPT_NUMBER);
+
+    FTM_StartTimer(BOARD_FTM_BASEADDR, kFTM_SystemClock);
+}
+/*******************************************************************************
+ * Other Code
+ ******************************************************************************/
+void delay(void)
+{
+    volatile uint32_t i = 0U;
+    for (i = 0U; i < 80000U; ++i)
+    {
+        __asm("NOP"); /* delay */
+    }
+}
+
+void update_duty_cycle(uint8_t updated_duty_cycle)
+{
+	FTM_DisableInterrupts(BOARD_FTM_BASEADDR, FTM_CHANNEL_INTERRUPT_ENABLE);
+
+	/* Disable channel output before updating the dutycycle */
+	FTM_UpdateChnlEdgeLevelSelect(BOARD_FTM_BASEADDR, BOARD_FTM_CHANNEL, 0U);
+
+	/* Update PWM duty cycle */
+	FTM_UpdatePwmDutycycle(BOARD_FTM_BASEADDR, BOARD_FTM_CHANNEL, kFTM_CenterAlignedPwm, updated_duty_cycle);
+
+	/* Software trigger to update registers */
+	FTM_SetSoftwareTrigger(BOARD_FTM_BASEADDR, true);
+
+	/* Start channel output with updated dutycycle */
+	FTM_UpdateChnlEdgeLevelSelect(BOARD_FTM_BASEADDR, BOARD_FTM_CHANNEL, PWM_LEVEL);
+
+	/* Delay to view the updated PWM dutycycle */
+	delay();
+
+	/* Enable interrupt flag to update PWM dutycycle */
+	FTM_EnableInterrupts(BOARD_FTM_BASEADDR, FTM_CHANNEL_INTERRUPT_ENABLE);
+}
+
+/*!
+ * @brief Main function
+ */
+int main(void)
+{
+	init_board();
+	init_pwm(20000, 40); //start 20khz pwm at 40% duty cycle
+	int duty_cycles[] = {40, 60, 80};
+    uint8_t i = 0;
+    while(1)
+    {
+    	update_duty_cycle(duty_cycles[i]);//cycle through different pwm duty cycles
+    	i = (i+1)%3;
+
+    }
+}
+
+/*******************************************************************************
+ * Interrupt functions
+ ******************************************************************************/
+// Just clears the status flag. More functionality could be added here
+void FTM_0_HANDLER(void)
+{
+    ftmIsrFlag = true;
+    if ((FTM_GetStatusFlags(BOARD_FTM_BASEADDR) & FTM_CHANNEL_FLAG) == FTM_CHANNEL_FLAG)
+    {
+        /* Clear interrupt flag.*/
+        FTM_ClearStatusFlags(BOARD_FTM_BASEADDR, FTM_CHANNEL_FLAG);
+    }
+}
