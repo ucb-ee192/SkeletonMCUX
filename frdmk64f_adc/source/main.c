9b70e23c7e046bccd48d40eb0454c6841af98ee2
Varun Tolani <vatolani@gmail.com>
2018-02-10 17:42:49 -0800

    add adc example

---
+++ frdmk64f_adc/source/main.c
@@ -0,0 +1,163 @@
+/*
+ * The Clear BSD License
+ * Copyright (c) 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2017 NXP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided
+ * that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "fsl_debug_console.h"
+#include "board.h"
+#include "fsl_adc16.h"
+
+#include "clock_config.h"
+#include "pin_mux.h"
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define DEMO_ADC16_BASEADDR ADC0
+#define DEMO_ADC16_CHANNEL_GROUP 0U
+#define DEMO_ADC16_USER_CHANNEL 12U /* PTB2, ADC0_SE12 */
+
+#define DEMO_ADC16_IRQn ADC0_IRQn
+#define DEMO_ADC16_IRQ_HANDLER_FUNC ADC0_IRQHandler
+
+#define VREF_BRD 3.300
+#define SE_12BIT 4096.0
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/* Initialize ADC16 */
+static void ADC_Init(void);
+static void init_board(void);
+void read_ADC(void);
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+volatile bool g_Adc16ConversionDoneFlag = false;
+volatile uint32_t g_Adc16ConversionValue = 0;
+adc16_channel_config_t g_adc16ChannelConfigStruct;
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+void init_board()
+{
+	/* Board pin, clock, debug console init */
+	BOARD_InitPins();
+	BOARD_BootClockRUN();
+	BOARD_InitDebugConsole();
+}
+
+static void ADC_Init(void)
+{
+	EnableIRQ(DEMO_ADC16_IRQn);
+    adc16_config_t adc16ConfigStruct;
+
+    /* Configure the ADC16. */
+    /*
+     * adc16ConfigStruct.referenceVoltageSource = kADC16_ReferenceVoltageSourceVref;
+     * adc16ConfigStruct.clockSource = kADC16_ClockSourceAsynchronousClock;
+     * adc16ConfigStruct.enableAsynchronousClock = true;
+     * adc16ConfigStruct.clockDivider = kADC16_ClockDivider8;
+     * adc16ConfigStruct.resolution = kADC16_ResolutionSE12Bit;
+     * adc16ConfigStruct.longSampleMode = kADC16_LongSampleDisabled;
+     * adc16ConfigStruct.enableHighSpeed = false;
+     * adc16ConfigStruct.enableLowPower = false;
+     * adc16ConfigStruct.enableContinuousConversion = false;
+     */
+    ADC16_GetDefaultConfig(&adc16ConfigStruct);
+#if defined(BOARD_ADC_USE_ALT_VREF)
+    adc16ConfigStruct.referenceVoltageSource = kADC16_ReferenceVoltageSourceValt;
+#endif
+    ADC16_Init(DEMO_ADC16_BASEADDR, &adc16ConfigStruct);
+
+    /* Make sure the software trigger is used. */
+    ADC16_EnableHardwareTrigger(DEMO_ADC16_BASEADDR, false);
+#if defined(FSL_FEATURE_ADC16_HAS_CALIBRATION) && FSL_FEATURE_ADC16_HAS_CALIBRATION
+    if (kStatus_Success == ADC16_DoAutoCalibration(DEMO_ADC16_BASEADDR))
+    {
+        PRINTF("\r\nADC16_DoAutoCalibration() Done.");
+    }
+    else
+    {
+        PRINTF("ADC16_DoAutoCalibration() Failed.\r\n");
+    }
+#endif /* FSL_FEATURE_ADC16_HAS_CALIBRATION */
+
+    /* Prepare ADC channel setting */
+    g_adc16ChannelConfigStruct.channelNumber = DEMO_ADC16_USER_CHANNEL;
+    g_adc16ChannelConfigStruct.enableInterruptOnConversionCompleted = true;
+
+#if defined(FSL_FEATURE_ADC16_HAS_DIFF_MODE) && FSL_FEATURE_ADC16_HAS_DIFF_MODE
+    g_adc16ChannelConfigStruct.enableDifferentialConversion = false;
+#endif /* FSL_FEATURE_ADC16_HAS_DIFF_MODE */
+}
+void read_ADC(void){
+	g_Adc16ConversionDoneFlag = false;
+	ADC16_SetChannelConfig(DEMO_ADC16_BASEADDR, DEMO_ADC16_CHANNEL_GROUP, &g_adc16ChannelConfigStruct);
+
+	//Block until the ADC Conversion is finished
+	 while (!g_Adc16ConversionDoneFlag)
+	 {
+	 }
+}
+
+void DEMO_ADC16_IRQ_HANDLER_FUNC(void)
+{
+    g_Adc16ConversionDoneFlag = true;
+    /* Read conversion result to clear the conversion completed flag. */
+    g_Adc16ConversionValue = ADC16_GetChannelConversionValue(DEMO_ADC16_BASEADDR, DEMO_ADC16_CHANNEL_GROUP);
+    /* Add for ARM errata 838869, affects Cortex-M4, Cortex-M4F Store immediate overlapping
+      exception return operation might vector to incorrect interrupt */
+#if defined __CORTEX_M && (__CORTEX_M == 4U)
+    __DSB();
+#endif
+}
+
+/*!
+ * @brief Main function
+ */
+int main(void)
+{
+    float analog_voltage;
+    init_board();
+    ADC_Init();
+    PRINTF("\r\nADC Demo!\r\n");
+
+    while (1)
+    {
+        read_ADC();
+        analog_voltage = (float)(g_Adc16ConversionValue * (VREF_BRD / SE_12BIT));
+        PRINTF("\r\n\r\nADC Value: %d, ADC Voltage: %0.3f\r\n", g_Adc16ConversionValue, analog_voltage);
+
+    }
+}
